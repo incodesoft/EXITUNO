eccb874b8b1b7fb4e714608c8b9d90615d994be3
JuDaIncodesoft <168034561+JuDaIncodesoft@users.noreply.github.com>
2024-04-24 19:20:35 -0500

    Update source_orden_fabricacion.js

--- source_orden_fabricacion.js
+++ source_orden_fabricacion.js
@@ -667,7 +667,7 @@ function grabar_datos() {
                 $(this).find('td').eq(7).css("background-color", "white");
             }
             
-            if ((cant_almacen === '' || cant_almacen <= 0) and tipo ==='RE') {
+            if ((cant_almacen === '' || cant_almacen <= 0) && tipo ==='RE') {
                 alertify.error('Linea: ' + lina2 + ' ' + 'Falta Cantidad Invalida');
                 $(this).find('td').eq(8).css("background-color", "#F67280");
                 band = false;
