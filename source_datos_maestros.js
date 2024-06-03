9c953024e9d1247ad68a8f4a2dcb97f6f3ae420e
JuDaIncodesoft <168034561+JuDaIncodesoft@users.noreply.github.com>
2024-06-03 15:38:15 -0500

    Create source_datos_maestros.js

---
+++ source_datos_maestros.js
@@ -0,0 +1,60 @@
+function lista_claseA() {
+  $(document).ready(function () {
+
+    $.ajax({
+      beforeSend: function () {
+        $("#clase_articulo").html("Cargando...");
+      },
+      url: 'pone_clase_articulos.php',
+      type: 'POST',
+      data: null,
+      success: function (x) {
+        $("#clase_articulo").html(x);
+        $(".select2").select2();
+      },
+      error: function (jqXHR, estado, error) {
+      }
+    });
+  });
+}
+
+function lista_grupoA() {
+  $(document).ready(function () {
+
+    $.ajax({
+      beforeSend: function () {
+        $("#grupo_articulo").html("Cargando...");
+      },
+      url: 'pone_grupo_articulos.php',
+      type: 'POST',
+      data: null,
+      success: function (x) {
+        $("#grupo_articulo").html(x);
+        $(".select2").select2();
+      },
+      error: function (jqXHR, estado, error) {
+      }
+    });
+  });
+}
+
+
+function lista_grupoUnidad() {
+  $(document).ready(function () {
+
+    $.ajax({
+      beforeSend: function () {
+        $("#grupo_medida").html("Cargando...");
+      },
+      url: 'pone_grupo_unidad.php',
+      type: 'POST',
+      data: null,
+      success: function (x) {
+        $("#grupo_medida").html(x);
+        $(".select2").select2();
+      },
+      error: function (jqXHR, estado, error) {
+      }
+    });
+  });
+}
