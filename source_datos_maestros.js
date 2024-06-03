e1e82fb3d8df00ccb2f0ca69b147cafc4e18c9f0
JuDaIncodesoft <168034561+JuDaIncodesoft@users.noreply.github.com>
2024-06-03 17:14:24 -0500

    Update source_datos_maestros.js

--- source_datos_maestros.js
+++ source_datos_maestros.js
@@ -81,3 +81,42 @@ function lista_igv_ventas() {
     });
   });
 }
+
+
+function registrar_datos(){
+  numero_articulo = $("#num_articulo").val();
+  descripcion_articulo = $("#descripcion_articulo").val();
+  clase_articulo = $("#clase_articulo option:selected").val();
+  grupo_articulo = $("#grupo_articulo option:selected").val();
+  grupo_medida = $("#grupo_medida option:selected").val();
+  num_catalogo = $("#num_catalogo").val();
+  codigo_um_compras = $("#codigo_um_compras").val();
+  igv_compras = $("#igv_compras option:selected").val();
+  igv_ventas = $("#igv_ventas option:selected").val();
+  cod_um_recuento = $("#cod_um_recuento").val();
+  nom_um_recuento = $("#nom_um_recuento").val();
+
+
+  $.ajax({
+      beforeSend: function () {
+      },
+      url: 'registra_datos_articulos.php',
+      type: 'POST',
+      data: {numero_articulo:numero_articulo, descripcion_articulo:descripcion_articulo, clase_articulo:clase_articulo, grupo_articulo:grupo_articulo,
+            num_catalogo:num_catalogo, codigo_um_compras:codigo_um_compras, igv_compras:igv_compras, igv_ventas:igv_ventas, cod_um_recuento:cod_um_recuento,  nom_um_recuento:nom_um_recuento},
+      success: function (data) {
+      $("#num_articulo").val("");
+      $("#descripcion_articulo").val("");
+      $("#num_catalogo").val("");
+      $("#codigo_um_compras").val("");
+      $("#cod_um_recuento").val("");
+      $("#nom_um_recuento").val("");
+
+      },
+      error: function (jqXHR, estado, error) {
+       $("#errores").html('Error... ' + estado + '  ' + error);
+      }
+  });
+
+  
+}
