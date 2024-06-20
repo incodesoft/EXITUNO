338d06e3c142ebfb4475ddc35c08d88723b5e602
JuDaIncodesoft <168034561+JuDaIncodesoft@users.noreply.github.com>
2024-06-20 09:50:11 -0500

    Update source_eventos.js

--- source_eventos.js
+++ source_eventos.js
@@ -1,1 +1,95 @@
+function registrarEventos(){
+    eventos = $("#list_eventos option:selected").val();
+    fecha = $("#event_fecha_ahora").val();
+    hora_inicio = $("#event_hora_inicio").val();
+    hora_fin = $("#event_hora_fin").val();
+    observaciones = $("#even_observaciones").val();
+    nro_isograf =0;
+    docentry = 0;
+    maquina = $("#lista_maquina select").val();
+    bandera = true;
+    if(maquina ==='Seleccione'){
+        bandera= false;
+    }
+    if(bandera===true){
+        $.ajax({
+            url: "registrar_eventos.php",
+            type: "POST",
+            data: {
+                eventos:eventos, fecha:fecha, hora_inicio:hora_inicio, hora_fin:hora_fin, observaciones:observaciones, nro_isograf:nro_isograf, docentry:docentry,maquina:maquina
+            },
+            success: function (x) {
+                //listar_eventos(docentry, nro_isograf);
+            },
+            error: function (jqXHR, estado, error) { },
+        }); 
+    }
+         
+}
+
+
+
+
+function listar_eventos(docentry, nro_isograf){
+   
+     $.ajax({
+        url: "consulta_eventos_registrados.php",
+        type: "POST",
+        data: {
+            docentry:docentry, nro_isograf:nro_isograf
+        },
+        success: function (x) {
+            $("#tabla_eventos").html(x);
+            $("#tabla_eventos2").DataTable({
+                order: [[0, 'asc']]
+            });
+           
+        },
+        error: function (jqXHR, estado, error) { },
+    });
+}
+
+
+
+function lista_maquinaria() {
+
+    $.ajax({
+        beforeSend: function () {
+            //$("#list_maquinaria").html("Recuperando Lista ...");
+        },
+        url: 'consulta_sql_datos_maquinaria.php',
+        type: 'POST',
+        data: null,
+        success: function (x) {
+            //console.log(x);
+            
+            $("#list_maquinaria").html(x);
+             $(".select2").select2();
+
+        },
+        error: function (jqXHR, estado, error) {
+        }
+    });
+
+}
+
+
+
+
+function finalizar_evento(id){
+    nro_isograf = $("#iso_evento").val();
+    docentry = $("#doc_evento").val();
+    
+    $.ajax({
+        url: "finalizar_eventos.php",
+        type: "POST",
+        data: {
+            id
+        },
+        success: function (x) {
+             listar_eventos(docentry, nro_isograf);
+        },
+        error: function (jqXHR, estado, error) { },
+    });        
+}
 
