849a4b6be415991e7ba531e8341fd529a98280fb
JuDaIncodesoft <168034561+JuDaIncodesoft@users.noreply.github.com>
2024-06-27 12:19:11 -0500

    Update source_orden_fabricacion.js

--- source_orden_fabricacion.js
+++ source_orden_fabricacion.js
@@ -1062,9 +1062,13 @@ function calcular_total_item(input, linea) {
 function calcular_total_item2da_explo(input, linea) {
     var fila = obtenerFila3(input);
     cantidad_base = parseFloat($($("#table_explo2").find("tbody > tr")[fila]).children("td")[5].children[0].value);
-    cantidad_planificada = parseFloat($("#cant_planificada").val());
+    //cantidad_planificada = parseFloat($("#cant_planificada").val());
+    cantidad_planificada = parseFloat($($("#table_explo2").find("tbody > tr")[fila]).children("td")[6].children[0].value);
+    
     var monto = parseFloat((cantidad_base * cantidad_planificada)).toFixed(4);
     $($("#table_explo2").find("tbody > tr")[fila]).children("td")[6].children[0].value = monto
+
+     //$('#table_explo2 > tbody > tr').find("td").find('input[id="cantidad_item_explo2"]').val(monto);
 }
 
 
