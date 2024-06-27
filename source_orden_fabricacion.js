2792bb622757819ffcf59595efa118df4ae76111
JuDaIncodesoft <168034561+JuDaIncodesoft@users.noreply.github.com>
2024-06-27 11:28:17 -0500

    Update source_orden_fabricacion.js

--- source_orden_fabricacion.js
+++ source_orden_fabricacion.js
@@ -233,7 +233,7 @@ function llena_detalle_elementos(producto, id) {
 
 
 function validar_numero(event) {
-  console.log(event);
+  //console.log(event);
   var charCode = (event.which) ? event.which : event.keyCode;
   if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
     alertify.error('No puede ingresar letras');
